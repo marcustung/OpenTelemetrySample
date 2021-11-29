0b9ecab604cf30aabf10e25fb668cd853f3ae2b1
marcus <marcustung116@gmail.com>
2021-11-30 02:23:36 +0800

    add openTelemetry package

--- OpenTelemetrySample/Program.cs
+++ OpenTelemetrySample/Program.cs
@@ -1,12 +1,28 @@
 ﻿using System;
+using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
 
 namespace OpenTelemetrySample
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly ActivitySource MyActivitySource = new ActivitySource("MyCompany.MyProduct.MyLibrary");
+
+        public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+                .SetSampler(new AlwaysOnSampler())
+                .AddSource("MyCompany.MyProduct.MyLibrary")
+                .AddConsoleExporter()
+                .Build();
+
+            using (var activity = MyActivitySource.StartActivity("SayHello"))
+            {
+                activity?.SetTag("foo", 1);
+                activity?.SetTag("bar", "Hello, World!");
+                activity?.SetTag("baz", new int[] { 1, 2, 3 });
+            }
         }
     }
 }
