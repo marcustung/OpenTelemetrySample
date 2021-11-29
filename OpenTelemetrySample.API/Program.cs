3479073491eee74a9f5cae7c876d79266b7e2e28
marcus <marcustung116@gmail.com>
2021-11-30 02:31:09 +0800

    add API ptoject

---
+++ OpenTelemetrySample.API/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OpenTelemetrySample.API
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
