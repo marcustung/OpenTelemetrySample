413e3a988de487d87a04e54607a02f8dfb196147
marcus <marcustung116@gmail.com>
2021-12-01 05:36:10 +0800

    add API Sample project

--- OpenTelemetrySample.API/Program.cs
+++ OpenTelemetrySample.API/Program.cs
@@ -1,12 +1,53 @@
 ﻿using System;
+using System.Diagnostics;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Resources;
+using System.Threading.Tasks;
 
 namespace OpenTelemetrySample.API
 {
     class Program
     {
+        private static readonly ActivitySource MyActivitySource = new ActivitySource("MyCompany.MyProduct.MyLibrary");
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+               .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService("MyService"))
+               .AddSource("MyCompany.MyProduct.MyLibrary")
+               .AddZipkinExporter(zipkinOptions =>
+               {
+                   zipkinOptions.Endpoint = new Uri("http://localhost:9411/api/v2/spans");
+               })               
+               .Build();
+
+            DoSomeWork();
+            Console.WriteLine("Example work done"); 
+        }
+
+        static void DoSomeWork()
+        {
+            using (Activity a = MyActivitySource.StartActivity("SomeWork"))
+            {
+                StepOne();
+                StepTwo();
+            }
+        }
+
+        static void StepOne()
+        {
+            using (Activity a = MyActivitySource.StartActivity("StepOne"))
+            {
+                Task.Delay(500);
+            }
+        }
+
+        static void StepTwo()
+        {
+            using (Activity a = MyActivitySource.StartActivity("StepTwo"))
+            {
+                Task.Delay(1000);
+            }
         }
     }
 }
