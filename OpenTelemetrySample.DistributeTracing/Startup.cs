9ed392cecfcd3ebc2579c1c0763772a165516205
marcustung <marcus_tung@mayohr.com>
2021-12-19 21:24:06 +0800

    新增註解

--- OpenTelemetrySample.DistributeTracing/Startup.cs
+++ OpenTelemetrySample.DistributeTracing/Startup.cs
@@ -40,7 +40,8 @@ namespace OpenTelemetrySample.DistributeTracing
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            
+            // Use TraceMiddleware
             app.UseTraceId();
 
             app.UseHttpsRedirection();
